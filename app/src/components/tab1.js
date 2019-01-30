fe674a97b886052fce9ddacc1b90e97472b44a3c
Amer <toukan.amer@gmail.com>
2019-01-29 23:43:26 -0500

    bamazon app added

--- app/src/components/tab1.js
+++ app/src/components/tab1.js
@@ -15,10 +15,10 @@ class Tab1 extends React.Component{
      <p>NodeJS, Express Handlebars, Javascript, ORM, mySQL.</p>
      </CardText>
      <CardActions border>
-     <a href = "https://github.com/anneventures/FinalProject">
+     <a href = "https://github.com/amertoukan/burger">
      <Button colored>Github</Button>
      </a>
-    <a href = "https://polar-chamber-37107.herokuapp.com/">
+    <a href = "https://agile-hollows-27414.herokuapp.com/">
     <Button colored>Live Demo</Button>
     </a>
      
@@ -31,13 +31,15 @@ class Tab1 extends React.Component{
 
      {/*Card Two */}
      <Card shadow = {5} style = {{minWidth: '450', margin : 'auto'}}>
-     <CardTitle style = {{color: 'black', height: '176px', background: 'url("https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2000px-React-icon.svg.png")center/cover'}}> JS Project #2</CardTitle>
+     <CardTitle style = {{color: '#fff', height: '176px', background: 'url("https://github.com/amertoukan/bamazon/raw/master/images/bamazonCustomer2.png")center/cover'}}>Bamazon</CardTitle>
      <CardText>
-     Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt labore recusandae adipisci officia, quas reprehenderit eligendi consequuntur ipsa enim dolore optio quisquam, doloribus deleniti placeat non. Obcaecati quis iure quas. Architecto itaque, praesentium reiciendis id consectetur pariatur, ea recusandae nihil illo nobis eius porro delectus.
+     <p>Bamazon is a terminal store. It allows users to shop through the database. </p>
+     <p>The app then prompts the user to select the product number and the quantity and displays the user their total</p>
+     <hr/>
+     Node, Inquirer, Nodemon, dotEnv
      </CardText>
      <CardActions border>
      <Button colored>Github</Button>
-     <Button colored>Live Demo</Button>
      </CardActions>
      <CardMenu style ={{color:'fff'}}>
      <IconButton name='share'/>

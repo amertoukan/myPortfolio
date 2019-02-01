4ae0c6374d432be25c0c4824e003d1b17cf33494
Amer <toukan.amer@gmail.com>
2019-02-01 15:54:03 -0500

    heroku

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require ('express');
-const path = require (path);
+const path = require ('path');
 const PORT = process.env.PORT || 4002;
 
 const app = express();
@@ -16,4 +16,6 @@ app.get('/*', (req,res) => {
 })
 
 
-app.listen(PORT);
+app.listen(PORT, ()=>{
+    console.log("https://localhost/"+PORT)
+});
